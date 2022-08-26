48a8ed6b5a7140e58cfa56443398296bf32c6f12
tkpgamesch <112048298+tkpgamesch@users.noreply.github.com>
2022-08-26 14:21:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,23 @@
+import discord
 from discord.ext import commands
-from os import getenv
-import traceback
+client = discord.Client(intents=discord.Intents.default())
 
-bot = commands.Bot(command_prefix='/')
+@client.event
+async def on_voice_state_update(member, before, after):
+    if member.guild.id == 1008022746212618330:
+        channel = after.channel
+        try:
+            if before.channel.members == [] and not before.channel.id == 1012189739601899601:
+                if before.channel.category_id == 1008022746770448437:
+                    await before.channel.delete()
+        except:
+            pass
+    
+    if after.channel.id == 1012189739601899601:
+        guild = after.channel.guild
+        private_channels = discord.utils.get(guild.categories, id=1008022746770448437)
+        voice_channel = await guild.create_voice_channel(member.name, category=private_channels)
+        await member.move_to(voice_channel)
+        await voice_channel.set_permissions(member, connect=True, speak=True, move_mamber=True, manage_roles=True, manage_channels=True, view_channel=True)
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+client.run("MTAxMjE4ODg0MDU4NjM4NzU5Ng.Gdug7e.Q8kuJpsxLGzM6JqfrZLMvRolxlPv5XzYL8Bm0U")
